e2615c4b61384bc9797e1a331031a98150230b67
Darko-Dev1 <kitanoskibest@gmail.com>
2025-06-13 01:20:16 +0200

    more details

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 backgroundIMG = ["https://crownpointecommunities.com/wp-content/uploads/2017/06/sky-sunny-clouds-cloudy-scaled.jpg", "https://images.pexels.com/photos/2682122/pexels-photo-2682122.jpeg", "https://images.pexels.com/photos/391522/pexels-photo-391522.jpeg", "https://images.pexels.com/photos/436792/pexels-photo-436792.jpeg?cs=srgb&dl=pexels-dan-hamill-144328-436792.jpg&fm=jpg"]
   let conthour = 0
   let srue = 0
+
+let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 async function gettingWeather() {
 
   console.log("hey")
@@ -12,14 +14,16 @@ async function gettingWeather() {
     console.error("no work")
   }
 
+  const date = new Date();
   let hoursLeft = []
-
+  document.getElementById("today").innerText += ":  " + daysOfTheWeek[date.getDay()] + " " + date.getDate() + "/" + date.getMonth()
   document.getElementById("loading").style.display = "none"
   document.querySelector("main").style.opacity = "100%"
   document.querySelector("#Wweek").style.opacity = "100%"
   document.querySelector("#Wtoday").style.opacity = "100%"
   setInterval( () => {  const date = new Date(); minutes = date.getMinutes();   let hour = date.getHours();   const year = date.getFullYear();   const month = date.getMonth() + 1;   const day = date.getDate();       
     let fullDate = `${year}-0${month}-${day}T${hour}:00`;
+    
     if (hour.toString.length < 2) {
       fullDate = `${year}-0${month}-${day}T0${hour}:00`;
     }else {
@@ -32,16 +36,16 @@ async function gettingWeather() {
       }
       document.querySelector("h1").innerText = `${a["hourly"]["temperature_2m"][indexTemp]}${a["hourly_units"]["temperature_2m"]}` 
       if(a["hourly"]["temperature_2m"][indexTemp] < 12 && a["hourly"]["temperature_2m"][indexTemp] > 0 ) {
-        document.querySelector("h4").innerText = `Feels chill ${hour}:${minutes}`
+        document.querySelector("h4").innerText = `Feels chill at ${hour}:${minutes}`
         document.querySelector("body").style.backgroundImage = `url(${backgroundIMG[1]})`
       } else if (a["hourly"]["temperature_2m"][indexTemp] < 0){
-        document.querySelector("h4").innerText = `Feels cold ${hour}:${minutes}`
+        document.querySelector("h4").innerText = `Feels cold at ${hour}:${minutes}`
         document.querySelector("body").style.backgroundImage = `url(${backgroundIMG[3]})`
       } else if (a["hourly"]["temperature_2m"][indexTemp] > 20) {
-        document.querySelector("h4").innerText = `Feels warm ${hour}:${minutes}`
+        document.querySelector("h4").innerText = `Feels warm at ${hour}:${minutes}`
         document.querySelector("body").style.backgroundImage = `url(${backgroundIMG[0]})`
       } else {
-        document.querySelector("h4").innerText = `Feels okay ${hour}:${minutes}`
+        document.querySelector("h4").innerText = `Feels okay at ${hour}:${minutes}`
         document.querySelector("body").style.backgroundImage = `url(${backgroundIMG[2]})`
       }
       const Wtoday = document.getElementById("Wtoday")
